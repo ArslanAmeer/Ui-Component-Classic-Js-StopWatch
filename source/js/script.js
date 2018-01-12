559f885debc94bd1a62d0201183cbe187d990bf0
Arslan Ameer <34485088+ArslanAmeer@users.noreply.github.com>
2018-01-12 14:48:24 +0500

    Added master Files
    Html , CSS & JS Files Uploaded

---
+++ source/js/script.js
@@ -0,0 +1,23 @@
+
+document.addEventListener('DOMContentLoaded',function () {
+
+    var interval = null;    
+    var timer = null;    
+
+    function timerValue(){
+        document.getElementById("timer").innerHTML = timer++;
+    }
+
+    document.getElementById('start').addEventListener('click',function(){
+        clearInterval(interval);
+        timer = 0;
+        interval = setInterval(timerValue,1000)
+    },false)
+
+    document.getElementById('stop').addEventListener('click',function(){
+        clearInterval(interval);
+    },false)
+
+});
+
+    
