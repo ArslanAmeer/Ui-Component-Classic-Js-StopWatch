a41a192e596409268f78d5a4d6092843001f1779
Hannan Akbar <hannanakbar@outlook.com>
2018-01-14 02:09:08 +0500

    Format code, minor improvements

--- source/js/script.js
+++ source/js/script.js
@@ -1,23 +1,21 @@
 
-document.addEventListener('DOMContentLoaded',function () {
+document.addEventListener('DOMContentLoaded', function () {
 
-    var interval = null;    
-    var timer = null;    
+    var interval = null;
+    var timer = 0;
 
-    function timerValue(){
+    function timerValue() {
         document.getElementById("timer").innerHTML = timer++;
     }
 
-    document.getElementById('start').addEventListener('click',function(){
+    document.getElementById('start').addEventListener('click', function () {
         clearInterval(interval);
         timer = 0;
-        interval = setInterval(timerValue,1000)
-    },false)
+        interval = setInterval(timerValue, 1000)
+    }, false);
 
-    document.getElementById('stop').addEventListener('click',function(){
+    document.getElementById('stop').addEventListener('click', function () {
         clearInterval(interval);
-    },false)
+    }, false);
 
 });
-
-    
